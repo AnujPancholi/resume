b6e3c6c66e7f6f3343975ecc73db79ccf8e559d9
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2021-08-07 20:29:22 +0530

    feat: embed links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 
 import StarOutlineOutlinedIcon from '@material-ui/icons/StarOutlineOutlined';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 import services from '../services/services.js';
 
 import '../flex.css';
+import '../App.css';
 import './style/footer.css';
 
 const Footer = () => {
@@ -18,13 +20,21 @@ const Footer = () => {
   }, []);
 
   return (
-    <footer className="text-keyname text-regular flex-container flex-center footer">
+    
+    <footer>
+      <a href={resumeUrl} className="text-keyname text-regular flex-container flex-center footer">
       Like it? Give it a&nbsp;
       <span className="flex-container flex-center">
         {<StarOutlineOutlinedIcon fontSize="inherit" />}
       </span>
-      ! {resumeUrl}
+      &nbsp;on&nbsp;
+      <span className="flex-container flex-center">
+        {<GitHubIcon fontSize="inherit" />}
+      </span>
+      &nbsp;({resumeUrl})
+      </a>
     </footer>
+    
   );
 };
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,19 +2,21 @@ import React, { useState, useEffect } from 'react';
 
 import '../flex.css';
 import './style/projects.css';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 import services from '../services/services.js';
+
 
 const Project = ({ project }) => {
   return (
     <div className="project-container">
       <div className="flex-container flex-horizontal">
         <div className="flex-auto text-keyname text-regular">
-          {project.title}
+          {project.title} {project.link && <a className="text-keyname text-regular" href={project.link}><GitHubIcon fontSize="inherit" /></a>}
         </div>
-        <div className="flex-container flex-horizontal flex-end flex-auto text-keyname text-small">
+        {/* <div className="flex-container flex-horizontal flex-end flex-auto text-keyname text-small">
           {project.link || ''}
-        </div>
+        </div> */}
       </div>
       <div className="flex-container flex-horizontal">
         <div className="project-description text-literal flex-grow-2">
